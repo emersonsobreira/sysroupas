6dd9249bad5e77ea0b6696d6580eb31b151e43f0
emersonsobreira <emersonsobreira528@gmail.com>
2023-05-24 10:58:33 -0300

    fornecedor e produtos estilizados

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   resources :itensvendas
   devise_for :users
   resources :users
-  resources :produtos
+  resources :produtos, :path => "produtos" do
+    collection do
+      get "/relatorio"  => "produtos#relatorio", :as => "relatorio" 
+    end
+  end
   resources :vendas
   resources :fornecedors
+
   root "produtos#index"
 end
+
